012fd74989577f228dc3bda998b84e7d669a75c8
Maks Sudoł <maksssudol@gmail.com>
2023-06-05 23:46:30 +0200

    #1 - Added registration and login for user closes

---
+++ LoginRegistration/Models/UserRegisterRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoginRegistration.Models
+{
+    public class UserRegisterRequest
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, MinLength(6)]
+        public string Password { get; set; }
+
+        [Required, Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
